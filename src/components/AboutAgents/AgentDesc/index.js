635b2b7e71484791d54e8eab173ab7b5dcf55ab1
Antoine Damiron <antoine@antoine.home>
2020-06-01 23:52:54 +0200

    merged request from Neomaking to fix arrow

---
+++ src/components/AboutAgents/AgentDesc/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from 'src/components/Nav';
+import AgentsBackground from './AgentsBackground';
+
+
+import './style.scss';
+
+const AgentsDesc = () => (
+  <>
+    <AgentDesc />
+  </>
+);
+
+export default AgentsDesc;
---
+++ src/components/AboutAgents/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from 'src/components/Nav';
+import AgentsBackground from './AgentsBackground';
+
+
+import './style.scss';
+
+const AboutAgents = () => (
+  <>
+    <Nav />
+    <AgentsBackground />
+    <AgentDesc />
+  </>
+);
+
+export default AboutAgents;
